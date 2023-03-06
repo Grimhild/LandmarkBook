978d00abc657a6d5948e89ce8699760c2a650f7d
Grimhild <ufukpolat9@gmail.com>
2023-03-06 18:33:01 +0300

    Landmark App Bitti

---
+++ LandmarkBook/DetailsVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsVC.swift
+//  LandmarkBook
+//
+//  Created by Ufuk on 6.03.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var landmarkLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+        
+    }
+    
+
+
+}
--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -11,29 +11,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBOutlet weak var tableView: UITableView!
     
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
+    var chosenLandmarkName = ""
+    var chosenLandmarkImage = UIImage()
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        landmarkNames.append("Colosseum")
+        landmarkNames.append("Great Wall")
+        landmarkNames.append("Kremlin")
+        landmarkNames.append("Stonhenge")
+        landmarkNames.append("Taj Mahal")
+        
+        landmarkImages.append(UIImage(named: "colosseum.jpg")!)
+        landmarkImages.append(UIImage(named: "gratewall.jpeg")!)
+        landmarkImages.append(UIImage(named: "kremlin.jpeg")!)
+        landmarkImages.append(UIImage(named: "stonehenge.jpeg")!)
+        landmarkImages.append(UIImage(named: "tacmahal.jpeg")!)
+
+        
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
 //        cell.textLabel?.text = "test"
         var content = cell.defaultContentConfiguration()
-        content.text = " Test"
+        content.text = landmarkNames[indexPath.row]
+//        content.image = landmarkImages[indexPath.row]
         cell.contentConfiguration = content
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenLandmarkName = landmarkNames [indexPath.row]
+        chosenLandmarkImage = landmarkImages[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.selectedLandmarkName = chosenLandmarkName
+            destinationVC.selectedLandmarkImage = chosenLandmarkImage
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.landmarkNames.remove(at: indexPath.row)
+            self.landmarkImages.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+            
+    }
     
 }
 
